c9517ad160d074ab0ddaa975e08ff3d720259a0f
d-forbes <46336303+d-forbes@users.noreply.github.com>
2024-01-17 13:42:28 -0600

    Base ast & parser

---
+++ parser/parser.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"github.com/d-forbes/go-interpreter/ast"
+	"github.com/d-forbes/go-interpreter/lexer"
+	"github.com/d-forbes/go-interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.l.NextToken()
+	return p
+}
+
+func (p *Parser) NextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
